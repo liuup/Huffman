ecf521bb2a3c65a5f5d579c240f62fee7d45192b
liuup <fyrwls@163.com>
2020-12-21 19:11:49 +0800

    add task description doc

---
+++ Huffman.cpp
@@ -0,0 +1,5 @@
+//
+// Created by liuup on 2020/12/21.
+//
+
+#include "Huffman.h"
---
+++ Huffman.h
@@ -0,0 +1,15 @@
+//
+// Created by liuup on 2020/12/21.
+//
+
+#ifndef MAIN_HUFFMAN_H
+#define MAIN_HUFFMAN_H
+
+
+class Huffman
+{
+
+};
+
+
+#endif //MAIN_HUFFMAN_H
