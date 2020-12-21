ebc924ac2e9d89787675f72893374bd2e0baf4c3
liuup <fyrwls@163.com>
2020-12-21 20:18:17 +0800

    修改huffman数据结构

--- Huffman.cpp
+++ Huffman.cpp
@@ -1,5 +1,67 @@
-//
-// Created by liuup on 2020/12/21.
-//
-
 #include "Huffman.h"
+
+// @brief 构造函数
+Huffman::Huffman()
+{
+    huffHeadPtr = nullptr; // 头节点指向空
+}
+
+
+// @brief 析构函数
+Huffman::~Huffman()
+{
+    delete huffHeadPtr;    // 删除头节点
+}
+
+// @brief 输入Huffman映射表
+void Huffman::getMap()
+{
+    char flag;  // 判断回车标志
+    char input_char;    // 输入的字符
+    int input_weight;  // 输入的权重
+
+    while(true)
+    {
+        cin >> input_char;
+        huffman_map.insert(pair<char, int>(input_char, -1));    // 插入字符
+
+        if((flag = getchar()) == '\n')
+            break;
+    }
+
+    for(iter = huffman_map.begin(); iter != huffman_map.end(); iter++)
+    {
+        cin >> input_weight;
+        iter->second = input_weight;
+
+        flag = getchar();   // 吸收逗号
+    }
+}
+
+// @brief 打印Huffman映射表
+void Huffman::showMap()
+{
+    for(iter = huffman_map.begin(); iter != huffman_map.end(); iter++)
+    {
+        cout << iter->first << " " << iter->second << endl;
+    }
+}
+
+// @brief
+void Huffman::encodeElement()
+{
+
+}
+
+// @brief
+void Huffman::decodeElement()
+{
+
+
+}
+
+// @brief 创建Huffman树
+void Huffman::createTree()
+{
+
+}
--- Huffman.h
+++ Huffman.h
@@ -1,13 +1,45 @@
-//
-// Created by liuup on 2020/12/21.
-//
-
 #ifndef MAIN_HUFFMAN_H
 #define MAIN_HUFFMAN_H
 
+#include <iostream>
+#include <cstdio>
+#include <queue>
+#include <vector>
+#include <map>
+#include <string>
+
+using namespace std;
+
+struct HuffNode
+{
+    char data;  // 信息
+    int weight; // 权值
+    HuffNode * left;    // 左节点
+    HuffNode * right;   // 右节点
+};
 
 class Huffman
 {
+public:
+    Huffman();
+    ~Huffman();
+
+    void getMap();  // 输入Huffman映射表
+    void showMap(); // 显示Huffman映射表
+
+    void encodeElement();  // 编码
+    void decodeElement();  // 解码
+
+    void createTree();  // 创建Huffman
+
+private:
+    map<char, int> huffman_map; // Huffman映射表
+    map<char, int>::iterator iter;  // Huffman映射表遍历
+
+    Huffman * huffHeadPtr;  // Huffman头节点
+
+
+
 
 };
 
